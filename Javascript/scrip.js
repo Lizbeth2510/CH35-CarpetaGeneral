42d456b1674b84864c7e09b6be9b50fea77dc25b
Lizbeth2510 <lizbethale251098>
2023-12-08 16:03:57 -0600

    Repositorio general

---
+++ Javascript/scrip.js
@@ -0,0 +1,70 @@
+//Comentarios de una sola línea
+
+/*Comentarios
+ multilinea*/
+
+
+ //alert("Hola mundo");
+
+//  console.log("Este es mi mensaje para mi consola");
+
+// var nombre = prompt ("Ingresa tu nombre");
+// alert("Hola, " + nombre + " Bienvenido a la CH35");
+
+// var numero1 = parseInt (prompt("Ingresa el primer número:" ));
+// var numero2 = parseInt (prompt("Ingresa el segundo número:"));
+
+// console.log ("Suma:" + (numero1 + numero2));
+// console.log ("Resta:" + (numero1 - numero2));
+// console.log ("Multiplicación:" + (numero1 * numero2));38
+
+// console.log ("División:" + (numero1 / numero2));1
+
+// var edad = prompt("Ingresa tu edad:");
+
+// if (edad >= 18) {
+//     alert("Bienvenido a Somos Poker. ¡Disfruta de la página aun eres joven!");
+// } else {
+//     alert("Lo siento, debes ser mayor de 18 años para acceder a Somos Poker. Aun eres un bebé.");
+// }
+
+// var edad = parseInt (prompt("¿Cuántas primaveras?"));
+// if (edad>=50){
+//   alert("Lo siento! Estas por caducar"); 
+// }
+  
+// else if (edad>=35){
+//     alert("Ya casi eres un anciano");
+// }
+  
+// else if (edad>=18){
+// alert("Ya dejaste de ser un bebé");
+// }
+// else {
+// alert("sorry! todavía necesitas cuidado de mamá"); 
+// }
+
+
+
+
+
+//TIPOS DE DATOS Y VARIABLES
+
+/*scope es el alcance de nuestras variables
+var, let, const: La diferencia es el alcance
+    var: tiene un alcance global
+    let: tiene un alcance local
+    const: se mantiene sin ningún cambio*/
+
+/*string:cadena de texto (lleva comillas)
+number: numero
+noolena: true/false
+null:nulo
+undefinied:no esta asigando o definido*/
+
+let nombre = "Daniel";
+let invitadosExtras = 2;
+let esMayorEdad = true;
+let edad = 24;
+let invitadoEspecial = null;
+let horaDeSalida = undefined;
